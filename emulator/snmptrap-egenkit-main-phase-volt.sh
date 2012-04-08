ea6bcf96e8e7e92b31b98caa74e2701c378014f5
Arif Kusbandono <arif.working@gmail.com>
2012-04-08 18:40:39 +0700

    v1.1 SNMP v2 sending trap emulator taking 1 plus 1 optional argument

--- emulator/snmptrap-egenkit-main-phase-volt.sh
+++ emulator/snmptrap-egenkit-main-phase-volt.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 #
-# v1.0
+# v1.1
 # simulate trap: Power Instruments Egenkit (SNMP v2c)
 # items	:	multiple items with string
 #
-# usage: ./snmptrap-env-mini-smoke.sh <trap receiver> <trap sender>
+# usage: ./snmptrap-egenkit-main-phase-volt.sh <trap receiver> <trap value>
 #
 
+defaultVal="232.00"
 trapReceiver="localhost"
-trapSender="localhost"
+trapVal=$defaultVal
 
 if [ "$1" != "" ]; then
 	if [ "$2" != "" ]; then
-		trapSender="$2"
+		trapVal="$2"
 		trapReceiver="$1"
 	else
+		# value of trap will become default here
 		trapReceiver="$1"
 	fi
 fi
@@ -22,8 +24,8 @@ fi
 snmptrap -v 2c -c public $trapReceiver \
 "" SNMPv2-SMI::enterprises.38337.1.1.3.7.3 \
 SNMPv2-SMI::enterprises.38337.1.1.3.7.3.1 \
-s "Tue Sep 20 14:56:01 2011" \
+s "`date +%a\ %b\ %d\ %T\ %Y`" \
 SNMPv2-SMI::enterprises.38337.1.1.3.7.3.2 \
 s "Mains phase voltage L3 (V)" \
 SNMPv2-SMI::enterprises.38337.1.1.3.7.3.3 \
-s "232.00"
+s $trapVal
