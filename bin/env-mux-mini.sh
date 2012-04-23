656739d78a737b26ec3b53f3ea6802cb0d299913
Arif Kusbandono <arif.working@gmail.com>
2012-04-23 10:51:18 +0700

    v2.0 adding EVENT auto acknowledge for incoming OK trigger (clearing previous PROBLEM trigger)

--- bin/env-mux-mini.sh
+++ bin/env-mux-mini.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 # env-mux-mini.sh
-# v1.1
+# v2.0
 # Arif Kusbandono
 #
 # requires:
 # 1. awk, sed
 # 2. snmptranslate from NET-SNMP
 # 3. NIT Enviromux MIB installed in path (/usr/share/snmp/mibs)
+# 4. zabbix_event_ack.py which requires Python 2.6
+#    (Please specify Python 2.6 bin)
 #
 # normal usage: ./env-mux-mini.sh
 # usage for checking parsed items to stdout: ./env-mux-mini.sh check
 #
+# To work exclusively with Enviromux Mini & Zabbix, the normal trap
+# after incident is compromised to be seen from Zabbix as change of
+# status (from PROBLEM (1) to OK (0))
+#
+# 1. To be seen as OK, the string must not contain the word "Alert"
+# 2. & the other way around
+# 3. The same Key of Zabbix trigger must be used, therefore `Ret` must
+#    removed from each key expression i.e. `waterSensor1RetTrap.0`
+#    becomes `waterSensor1Trap.0`
 
 unset checkStatus
 if [ "$1" == "check" ]; then
@@ -21,6 +32,9 @@ MIB=NETWORK-TECHNOLOGIES-GLOBAL-REG
 ZABBIX_SERVER="localhost";
 ZABBIX_PORT="10051";
 ZABBIX_SENDER="/home/zabbix/bin/zabbix_sender";
+PYTHON26="/usr/bin/python26"
+API_DIR="/home/zabbix/bin/api"
+ACK_API="$API_DIR/zabbix_event_ack.py";
 
 read hostname 
 vars=
@@ -60,11 +74,29 @@ key=`snmptranslate -m +$MIB $keyOID`
 key=`echo -n $key | awk -F'::' '{print $2}'`
 str=$ori_str
 
-#send to zabbix
-$ZABBIX_SENDER -z $ZABBIX_SERVER -p $ZABBIX_PORT -s $HOST -k $key -o "$str"
+# evaluate before sending to zabbix: whether it is incident trap or
+# normal trap
+incidentStatus=`echo $key | grep "Ret" | wc -l`
+if [ $incidentStatus -gt 0 ]; then
+	vkey=`echo $key | sed -e 's/Ret//g'`
+	vstr=`echo $str | sed -e 's/Alert//g'`
+	
+	key=$vkey
+	
+	# put things to do (script) before sending them to zabbix below:
+	cd $API_DIR
+	ack_eventid=`$PYTHON26 $ACK_API $key $HOST.dic`
+	echo $ack_eventid
+	str=`echo $vstr "acknowledge event " $ack_eventid`
+fi
 
+
+# send to zabbix with -vv and stdout debug or clean
 if [ "$checkStatus" == "yes" ]; then
 	echo $str
 	echo $key
 	echo $HOST
+	$ZABBIX_SENDER -vv -z $ZABBIX_SERVER -p $ZABBIX_PORT -s $HOST -k $key -o "$str"
+else
+	$ZABBIX_SENDER -z $ZABBIX_SERVER -p $ZABBIX_PORT -s $HOST -k $key -o "$str"
 fi
